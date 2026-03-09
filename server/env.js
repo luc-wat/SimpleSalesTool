da7498ea4eb67d7e268400d7554e58735701b2d5
luc_wat <lucwat04@gmail.com>
2026-03-09 22:48:52 +0000

    Add database

---
+++ server/env.js
@@ -0,0 +1,12 @@
+import { config } from "dotenv";
+import { cleanEnv, port, str } from "envalid";
+
+config({ path: ".env" });
+
+export const env = cleanEnv(process.env, {
+  POSTGRES_USER: str(),
+  POSTGRES_PASSWORD: str(),
+  POSTGRES_HOST: str(),
+  POSTGRES_PORT: port({ default: 5432 }),
+  POSTGRES_DB: str(),
+});
